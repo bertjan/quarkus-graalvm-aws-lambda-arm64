a9efed03bf7fd73255c661df3509a1947dca56dd
Bert Jan Schrijver <bertjan@openvalue.nl>
2025-05-20 14:21:37 +0200

    upgrade to Quarkus 3.22.3, stick with UBI 8 container build image for now

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn package -Dnative -Dquarkus.native.container-build=true
+mvn package -Dnative -Dquarkus.native.container-build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:jdk-21
